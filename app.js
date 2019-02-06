cadebe9cec204c7a276a31b434f28e46cbba4523
Taha haq <haqtah@gmail.com>
2019-02-06 15:13:16 +0500

    v1 of server

---
+++ app.js
@@ -0,0 +1,29 @@
+let express = require("express"),
+    methodOverride = require("method-override"),
+    app = express(),
+    constants = require('./utils/constant'),
+    utilsFunctions = require('./utils/functions'),
+    cors = require('cors'),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+let emailRoutes = require("./routes/email");
+
+// App Config
+
+mongoose.connect("mongodb://psw:qweasd123@ds223605.mlab.com:23605/psw_db");
+
+app.use(bodyParser.json({extended: true}));
+app.use(cors());
+app.use(methodOverride("_method"));
+
+
+app.use('/email', emailRoutes);
+
+
+const port = process.env.PORT || 7001;
+app.listen(port);
+
+console.log(`PSW Server listening on ${port}`);
+
+module.exports.app = app;
