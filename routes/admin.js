e01ccd8d632d45c7169c9978f08db1d622b3d49e
taha <haqtaha@gmail.com>
2019-03-09 14:11:10 +0500

    token bug fixed

--- routes/admin.js
+++ routes/admin.js
@@ -19,7 +19,7 @@ function verifyToken(req, res, next) {
     if (!token)
         return res.status(403).send({
             auth: false, message: 'No token provided.'});
-    jwt.verify(token, constants.secret.secret, function (err, decoded) {
+    jwt.verify(token, constants.secret, function (err, decoded) {
         if (err)
             return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
         // if everything good, save to request for use in other routes
